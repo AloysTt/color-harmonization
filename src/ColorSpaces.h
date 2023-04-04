a1ed85f99effd6a0d84d1c4f150a84c09259f816
Aloys <aloys.tetenoire@etu.umontpellier.fr>
2023-04-04 10:12:14 +0200

    Add regions grow

--- src/ColorSpaces.h
+++ src/ColorSpaces.h
@@ -45,5 +45,14 @@ void rgb_to_ycbcr(const unsigned char * image, float * imageYCbCr, int h, int w)
 void ycbcr_to_rgb(const ColorYCbCr & ycbcr, ColorRGB & rgb);
 
 float ycbcr_distance_relative_euclidean(float y1, float cb1, float cr1, float y2, float cb2, float cr2);
+double ycbcr_distance_relative_euclidean_squared(float y1, float cb1, float cr1, float y2, float cb2, float cr2);
+inline float ycbcr_distance_relative_euclidean(const float *px1, const float *px2)
+{
+	return ycbcr_distance_relative_euclidean(px1[0], px1[1], px1[2], px2[0], px2[1], px2[2]);
+}
+inline double ycbcr_distance_relative_euclidean_squared(const float *px1, const float *px2)
+{
+	return ycbcr_distance_relative_euclidean_squared(px1[0], px1[1], px1[2], px2[0], px2[1], px2[2]);
+}
 
 #endif // COLORSPACES_H
