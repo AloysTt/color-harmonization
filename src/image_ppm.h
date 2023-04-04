3b27419a45c577f09f444be77ae1a349af972beb
Aloys <aloys.tetenoire@etu.umontpellier.fr>
2023-04-04 10:11:47 +0200

    Add pgm image writing with 2 bytes

--- src/image_ppm.h
+++ src/image_ppm.h
@@ -167,6 +167,31 @@ void ecrire_image_pgm(char  nom_image[], OCTET *pt_image, int nb_lignes, int nb_
 		fclose(f_image);
 	}
 }
+
+void ecrire_image_pgm_2o(char nom_image[], unsigned short *pt_image, int nb_lignes, int nb_colonnes)
+{
+	FILE *f_image;
+	int taille_image = nb_colonnes * nb_lignes;
+
+	if( (f_image = fopen(nom_image, "wb")) == NULL)
+	{
+		printf("\nPas d'acces en ecriture sur l'image %s \n", nom_image);
+		exit(EXIT_FAILURE);
+	}
+	else
+	{
+		fprintf(f_image,"P5\r") ;                               /*ecriture entete*/
+		fprintf(f_image,"%d %d\r65535\r", nb_colonnes, nb_lignes) ;
+
+		if( (fwrite(pt_image, sizeof(unsigned short), taille_image, f_image))
+			!= (size_t) taille_image)
+		{
+			printf("\nErreur d'écriture de l'image %s \n", nom_image);
+			exit(EXIT_FAILURE);
+		}
+		fclose(f_image);
+	}
+}
 /*===========================================================================*/
 
 void lire_nb_lignes_colonnes_image_pgm(char nom_image[], int *nb_lignes, int *nb_colonnes)
